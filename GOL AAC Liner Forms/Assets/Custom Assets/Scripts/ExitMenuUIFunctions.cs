b477cc67ecaad1c884d61819c7d53078f9877d42
jasonjoyce971 <jasonjoyce971@hotmail.co.uk>
2017-08-09 19:18:07 +0100

    Normal Workflow Backup

---
+++ GOL AAC Liner Forms/Assets/Custom Assets/Scripts/ExitMenuUIFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitMenuUIFunctions : MonoBehaviour
+{
+    // Exit Panel Object
+    public GameObject Panel;
+
+    // Button Functions
+    public void CancelButtonClick ()
+    {
+        Destroy(Panel);
+        Debug.Log("Exit Panel: Destroy Exit Panel", Panel);
+    }
+
+    public void ConfirmButtonClick ()
+    {
+        Application.Quit();
+        Debug.Log("Exit Panel: Exit Application", Panel);
+    }
+}
